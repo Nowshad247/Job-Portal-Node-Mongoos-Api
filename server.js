c7579529f223deea184483e07e69708786af65b4
Nowshad247 <lewsedmond@gmail.com>
2022-11-05 20:30:15 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const color = require("colors");
+const app = require("./app");
+
+//Database connection 
+mongoose.connect()
